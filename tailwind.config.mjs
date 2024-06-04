cc93d8e88dd06ae76eab707be4ea81b2d6409f04
aidil-sekandar <aidiliskandar63@gmail.com>
2024-06-05 01:20:50 +0800

    design the main layout roughly

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				PlusJakartaSans: ["PlusJakartaSans", "sans-serif"],
+			},
+			colors: {
+				bodyColor: "#191919",
+				textSecondary: "#97979F",
+				textContent: "#c3c3c3",
+			}
+		},
 	},
-	plugins: [],
 }
